4631a0a8d5e1ae41dbb0526cdd3212a25000c71f
Blake Embrey <hello@blakeembrey.com>
2016-05-20 11:16:38 -0700

    Add root page so people stop emailing me

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -10,6 +10,14 @@ router.use('/search', search)
 router.use('/versions', versions)
 router.use('/entries', entries)
 
+router.get('/', function (req, res) {
+  res.send(
+    'Typings is currently up. Maybe you\'re after documentation? See ' +
+    'https://github.com/typings/typings or https://github.com/typings/api ' +
+    'for more information'
+  )
+})
+
 router.get('/health', function (req, res) {
   res.status(200).end()
 })
